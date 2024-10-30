ea64609287c3b5baf96253764288e6e69f46affa
RaulexG <raulrexchavira@gmail.com>
2024-10-30 12:29:42 -0600

    update file login

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './style.scss';
-import Header from './_cliente/Header';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Nav from './_cliente/Nav';
 import Hero from './_cliente/Hero';
 import Servicios from './_cliente/Servicios';
@@ -7,20 +7,30 @@ import Productos from './_cliente/Productos';
 import Citas from './_cliente/Citas';
 import Informacion from './_cliente/Informacion';
 import Footer from './_cliente/Footer';
-
+import Login from './_user/login/view';
 
 function App() {
   return (
+    <Router>
       <div className="app">
-        <Nav />        
-        <Hero />
-        <Servicios />
-        <Productos />
-        <Citas />
-        <Informacion />
-        <Footer />
-    </div>
+        <Nav />
+        <Routes>
+          <Route path="/" element={
+            <>
+              <Hero />
+              <Servicios />
+              <Productos />
+              <Citas />
+              <Informacion />
+              <Footer />
+            </>
+          } />
+          <Route path="/iniciar-sesion" element={<Login />} /> {/* Página de inicio de sesión */}
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
 export default App;
+
---
+++ src/_user/login/view.js
@@ -0,0 +1,11 @@
+function Login() {
+    return (
+      <div>
+        <h2>Iniciar Sesión</h2>
+        {/* Aquí puedes agregar el formulario de inicio de sesión */}
+      </div>
+    );
+  }
+  
+  export default Login;
+  
